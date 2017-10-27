b6b2a543a0ec7cea206e0b7cf2591a22ed27509f
Gokul-Sourav <gokulkumargkl@gmail.com>
2017-10-27 17:38:21 +0530

    Add files via upload

---
+++ Crud/js/Controllers/MainController.js
@@ -0,0 +1,19 @@
+
+
+app.controller('MainController',['$scope',function($scope){
+	
+$scope.title = 'Shopping Cart';
+$scope.products=[
+	{
+		Sno : 1,
+		Name: 'Lenovo vibe k5 note(3gb,32gb) goldknxcfgkhdfghkdfhgkhdfgkhgkhfgkhg',
+		Price: 11999
+	},
+	{
+		Sno : 2,
+		Name: 'Lenovo vibe k5 note(4gb,64gb) grey',
+		Price: 13999.7
+	}
+	];
+
+}]);
